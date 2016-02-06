e271cd0835addf96a67ba7647850e87f92b08089
Michael Gruner <michael.gruner@ridgerun.com>
2016-02-05 18:56:27 -0600

    Create base Autotools project
    This project ressembles a standard GStreamer Autotools project; was,
    in fact, born from the GStreamer core. This will allow us leverage
    many of the already developed testing, documenting, profiling,
    etc... capabilities.
    
    Some features were deliberately left behind until the state of the
    project requires its inclusion. Some of them are:
     - Gettext internationalization/localization capabilities
     - Project's .spec file
     - Win32 related stuff

---
+++ plugins/tracers/gstsharktracers.c
@@ -0,0 +1,38 @@
+/* GstShark - A Front End for GstTracer
+ * Copyright (C) 2016 RidgeRun Engineering <michael.gruner@ridgerun.com>
+ *
+ * This file is part of GstShark.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <gst/gst.h>
+
+static gboolean
+plugin_init (GstPlugin * plugin)
+{
+  /* if (!gst_tracer_register (plugin, "example", gst_example_tracer_get_type ())) */
+  /*   return FALSE; */
+
+  return TRUE;
+}
+
+GST_PLUGIN_DEFINE (GST_SHARK_VERSION_MAJOR, GST_SHARK_VERSION_MINOR,
+    sharktracers, "GstShark tracers", plugin_init, VERSION, GST_SHARK_LICENSE,
+    PACKAGE_NAME, PACKAGE_URL);
