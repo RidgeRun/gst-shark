dca0972a19c45713355d6d05c8183608e246ec1d
Sebastian Fatjo <sebastian.fatjo@ridgerun.com>
2016-02-12 17:46:25 -0600

    Remove unnecessary characters at the end of every line
    Unuseful characters (at the end every line) that were commited on the initial import and
    make the understanding of the code harder were removed.
    
    These characters were added automatically by the editor used at the moment of their
    development and they are not necessary.

--- plugins/tracers/gstdot.c
+++ plugins/tracers/gstdot.c
@@ -7,59 +7,58 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */  
-    
+ */
+
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
-#endif /*  */
-    
+#endif /*  */
+
 #include <gst/gst.h>
 #include <gvc.h>
 #include <cgraph.h>
-    
+
 #include "gstdot.h"
-const gchar *
-gst_dot_pipeline_to_string (const GstPipeline * pipe) 
-{
-  GstBin * bin;
-  GstDebugGraphDetails flags;
-   g_return_val_if_fail (pipe, NULL);
-   bin = GST_BIN (pipe);
-  flags = GST_DEBUG_GRAPH_SHOW_ALL;
-   return gst_debug_bin_to_dot_data (bin, flags);
-}
 
- gboolean 
-gst_dot_do_render (const gchar * dot_string, GstDotRender render,
-    gpointer args) 
+const gchar *
+gst_dot_pipeline_to_string (const GstPipeline * pipe)
 {
-  g_return_val_if_fail (dot_string, FALSE);
-  g_return_val_if_fail (render, FALSE);
-   return render (dot_string, args);
-}
+  GstBin *bin;
+  GstDebugGraphDetails flags;
+  g_return_val_if_fail (pipe, NULL);
+  bin = GST_BIN (pipe);
+  flags = GST_DEBUG_GRAPH_SHOW_ALL;
+  return gst_debug_bin_to_dot_data (bin, flags);
+}
 
- gboolean  gst_dot_x11_render (const gchar * dot_string, gpointer args) 
+gboolean
+gst_dot_do_render (const gchar * dot_string, GstDotRender render, gpointer args)
 {
-  Agraph_t * G;
-  GVC_t * gvc;
-   g_return_val_if_fail (dot_string, FALSE);
-   gvc = gvContext ();
-  G = agmemread (dot_string);
-  gvLayout (gvc, G, "dot");
-  gvRender (gvc, G, "x11", NULL);
-  gvFreeLayout (gvc, G);
-  agclose (G);
-  gvFreeContext (gvc);
-   return TRUE;
-}
+  g_return_val_if_fail (dot_string, FALSE);
+  g_return_val_if_fail (render, FALSE);
+  return render (dot_string, args);
+}
 
- 
+gboolean
+gst_dot_x11_render (const gchar * dot_string, gpointer args)
+{
+  Agraph_t *G;
+  GVC_t *gvc;
+  g_return_val_if_fail (dot_string, FALSE);
+  gvc = gvContext ();
+  G = agmemread (dot_string);
+  gvLayout (gvc, G, "dot");
+  gvRender (gvc, G, "x11", NULL);
+  gvFreeLayout (gvc, G);
+  agclose (G);
+  gvFreeContext (gvc);
+  return TRUE;
+}
--- plugins/tracers/gstdot.h
+++ plugins/tracers/gstdot.h
@@ -7,28 +7,27 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */  
-    
+ */
+
 #ifndef __GST_DOT_H__
 #define __GST_DOT_H__
-    
+
 #include <gst/gst.h>
-G_BEGIN_DECLS 
-    typedef gboolean (*GstDotRender) (const gchar * dot_string, gpointer args);
- const gchar *gst_dot_pipeline_to_string (const GstPipeline * pipe);
- gboolean 
+G_BEGIN_DECLS
+    typedef gboolean (*GstDotRender) (const gchar * dot_string, gpointer args);
+const gchar *gst_dot_pipeline_to_string (const GstPipeline * pipe);
+gboolean
 gst_dot_do_render (const gchar * dot_string, GstDotRender render,
     gpointer args);
- gboolean  gst_dot_x11_render (const gchar * dot_string, gpointer args);
- G_END_DECLS  
+gboolean gst_dot_x11_render (const gchar * dot_string, gpointer args);
+G_END_DECLS
 #endif //__GST_DOT_H__
-    
