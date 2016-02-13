14d209b5d32681680f7640bc4afdef4c9e9319bf
Sebastian Fatjo <sebastian.fatjo@ridgerun.com>
2016-02-12 18:08:48 -0600

    Add a new thread function for displaying the pipeline graphic
    A new function was added to the source code to create new thread of execution
    when the function for graphic the pipeline is being call.
    
    Because of the need that the pipeline graphic had to run on another thread so the
    pipeline could continue running even when the window that displays the pipeline
    graphic is close.
    
    In order to do that the Glib libraries were used to create a new and independet
    thread that receives as a parameter a structure with all the parameters included,
    including a pointer to the graphic function.

--- plugins/tracers/gstdot.c
+++ plugins/tracers/gstdot.c
@@ -28,23 +28,74 @@
 
 #include "gstdot.h"
 
+typedef struct _GstDotRenderThread GstDotRenderThread;
+
+struct _GstDotRenderThread
+{
+  GstDotRender render;
+  const gchar *dot_string;
+  gpointer args;
+};
+
 const gchar *
 gst_dot_pipeline_to_string (const GstPipeline * pipe)
 {
   GstBin *bin;
   GstDebugGraphDetails flags;
+
   g_return_val_if_fail (pipe, NULL);
   bin = GST_BIN (pipe);
   flags = GST_DEBUG_GRAPH_SHOW_ALL;
+
   return gst_debug_bin_to_dot_data (bin, flags);
+}
+
+static gpointer
+gst_dot_render_thread (gpointer data)
+{
+  GstDotRenderThread *thread_args;
+  GstDotRender render;
+  const gchar *dot_string;
+  gpointer args;
+
+  g_return_val_if_fail (data, NULL);
+  thread_args = (GstDotRenderThread *) data;
+
+  /* Breaking the structure passed as a parameter */
+  render = thread_args->render;
+  dot_string = thread_args->dot_string;
+  args = thread_args->args;
+
+  /* Calling the function to get the pipeline graphic */
+  render (dot_string, args);
+
+  /* Freeing the memory allocated and killing the graphic thread */
+  g_free (thread_args);
+  g_thread_exit (0);
+
+  return NULL;
 }
 
 gboolean
 gst_dot_do_render (const gchar * dot_string, GstDotRender render, gpointer args)
 {
+  GstDotRenderThread *thread_args;
+
   g_return_val_if_fail (dot_string, FALSE);
   g_return_val_if_fail (render, FALSE);
-  return render (dot_string, args);
+
+  /* Allocating memory with the parameters for the graphic pipeline */
+  thread_args = g_malloc (sizeof (GstDotRenderThread));
+
+  /* Filling the structure that is going to be used to get a new thread
+     for displaying the pipeline graphic */
+  thread_args->render = render;
+  thread_args->dot_string = dot_string;
+  thread_args->args = args;
+
+  g_thread_new ("GstDotRender", gst_dot_render_thread, thread_args);
+
+  return TRUE;
 }
 
 gboolean
@@ -52,6 +103,7 @@ gst_dot_x11_render (const gchar * dot_string, gpointer args)
 {
   Agraph_t *G;
   GVC_t *gvc;
+
   g_return_val_if_fail (dot_string, FALSE);
   gvc = gvContext ();
   G = agmemread (dot_string);
@@ -60,5 +112,6 @@ gst_dot_x11_render (const gchar * dot_string, gpointer args)
   gvFreeLayout (gvc, G);
   agclose (G);
   gvFreeContext (gvc);
+
   return TRUE;
 }
