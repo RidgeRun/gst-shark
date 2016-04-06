695cf1166fddb167df953932b76103124a4ec6c5
rrcarlosrodriguez <carlos.rodriguez@ridgerun.com>
2016-04-06 14:00:18 -0600

    Add feature for opening a dot file from a specific directory
    A JFileChooser method was added in order to allow to the user
    to open a desired dot file by searching for it in the computer
    directories. The absolute path of the choosen file is added to the
    command in charge of the conversion from dot to png image.

--- eclipse/com.ridgerun.gsttune/src/com/ridgerun/gsttune/handlers/ShowDotHandler.java
+++ eclipse/com.ridgerun.gsttune/src/com/ridgerun/gsttune/handlers/ShowDotHandler.java
@@ -1,7 +1,10 @@
 package com.ridgerun.gsttune.handlers;
 
 import java.awt.Dimension;
+import java.io.File;
+
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -9,17 +12,16 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
-
 /**
  * handler extends AbstractHandler, an IHandler base class.
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
 public class ShowDotHandler extends AbstractHandler {
-	
-	private static final String COMMAND = "dot -Tpng /home/caguero/0.00.02.742694709-gst-launch.PLAYING_READY.dot -o /home/caguero/output.png";
-	private static final String RESULTFILE = "/home/caguero/output.png";
-	
+
+	private static String COMMAND;
+	private static String RESULTFILE = "/tmp/output.png";
+
 	/**
 	 * The constructor.
 	 */
@@ -30,8 +32,25 @@ public class ShowDotHandler extends AbstractHandler {
 	 * the command has been executed, so extract extract the needed information
 	 * from the application context.
 	 */
+	ImageIcon image;
+
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
+
+		JFrame frame = new JFrame("");
+		JFileChooser fc = new JFileChooser();
+		fc.setPreferredSize(new Dimension(800,600));
+		int returnVal = fc.showOpenDialog(frame); //Where frame is the parent component
+
+		File file = null;
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+		    file = fc.getSelectedFile();
+		    //Now you have your file to do whatever you want to do
+		} else {
+		    //User did not choose a valid file
+		}
+
+		COMMAND = "dot -Tpng " + file.getAbsolutePath() + " -o " + RESULTFILE;
+
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec(COMMAND);
@@ -40,13 +59,20 @@ public class ShowDotHandler extends AbstractHandler {
 			e.printStackTrace();
 		}
 
-		ImageIcon image = new ImageIcon(RESULTFILE);
+		image = new ImageIcon(RESULTFILE);
+
         JLabel imageLabel = new JLabel(image);
-		JFrame frame = new JFrame(""); 
         frame.add(imageLabel);
         frame.setSize(new Dimension(image.getIconWidth(), image.getIconHeight()));
         frame.setVisible(true);
-        
+
+        try {
+            Thread.sleep(500);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        image.getImage().flush();
         return null;
 	}
 }
