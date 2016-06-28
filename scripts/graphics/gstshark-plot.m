cb65eef9fb6bba0a2f8ee101271681e7e5d73133
Manuel <manuel.leiva@ridgerun.com>
2016-06-28 14:06:54 -0600

    Remove figure counter variable
    This counter is not needed since there is a script to create and save
    the figures.

--- scripts/graphics/gstshark-plot.m
+++ scripts/graphics/gstshark-plot.m
@@ -5,7 +5,6 @@ GSTSHARK_SAVEFIG_FORMAT = 'pdf'
 TRUE = 1;
 
 arg_list = argv ();
-figs_num = 0;
 
 for i = 1:nargin
   
@@ -15,23 +14,18 @@ for i = 1:nargin
         case 'cpuusage'
             disp('Processing cpusage...')
             plot_cpuusage
-            figs_num = figs_num + 1;
         case 'framerate'
             disp('Processing framerate...')
             plot_framerate
-            figs_num = figs_num + 1;
         case 'proctime'
             disp('Processing proctime...')
             plot_proctime
-            figs_num = figs_num + 1;
         case 'interlatency'
             disp('Processing interlatency...')
             plot_interlatency
-            figs_num = figs_num + 1;
         case 'scheduling'
             disp('Processing scheduling...')
             plot_scheduling
-            figs_num = figs_num + 1;
         case '--savefig'
             GSTSHARK_SAVEFIG = TRUE;
         case 'png'
