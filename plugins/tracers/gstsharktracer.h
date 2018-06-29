63b53b710c1b7002c044d24a225a34d7a79053e1
Michael Gruner <michael.gruner@ridgerun.com>
2018-06-28 22:06:41 -0600

    Add empty base shark tracer

---
+++ plugins/tracers/gstsharktracer.h
@@ -0,0 +1,39 @@
+/* GstShark - A Front End for GstTracer
+ * Copyright (C) 2016-2018 RidgeRun Engineering <michael.gruner@ridgerun.com>
+ *
+ * This file is part of GstShark.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __GST_SHARK_TRACER_H__
+#define __GST_SHARK_TRACER_H__
+
+#include <gst/gst.h>
+#include <gst/gsttracer.h>
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_SHARK_TRACER (gst_shark_tracer_get_type())
+G_DECLARE_DERIVABLE_TYPE (GstSharkTracer, gst_shark_tracer, GST_SHARK, TRACER, GstTracer)
+
+struct _GstSharkTracerClass
+{
+  GstTracerClass parent_class;
+};
+
+G_END_DECLS
+
+#endif /* __GST_SHARK_TRACER_H__ */
