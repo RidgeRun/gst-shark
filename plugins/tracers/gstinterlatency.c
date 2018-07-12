46fff411634dd9b0f7239444fab32ac5c5ef58c0
Angel Phillips <angel.phillips@ridgerun.com>
2018-07-12 15:24:56 -0600

    Remove unused code

--- plugins/tracers/gstinterlatency.c
+++ plugins/tracers/gstinterlatency.c
@@ -37,10 +37,6 @@
  * latency.
  */
 
-#ifdef HAVE_CONFIG_H
-#  include "config.h"
-#endif
-
 #include "gstinterlatency.h"
 #include "gstctf.h"
 
@@ -63,9 +59,7 @@ static GQuark latency_probe_id;
 static GQuark latency_probe_pad;
 static GQuark latency_probe_ts;
 
-#ifdef GST_STABLE_RELEASE
 static GstTracerRecord *tr_interlatency;
-#endif
 
 static const gchar interlatency_metadata_event[] = "event {\n\
     name = interlatency;\n\
@@ -144,15 +138,7 @@ print_latency (GstInterlatencyTracer * self,
   time_string = g_string_new ("");
   g_string_printf (time_string, "%" GST_TIME_FORMAT, GST_TIME_ARGS (time));
 
-#ifdef GST_STABLE_RELEASE
   gst_tracer_record_log (tr_interlatency, src, sink, time_string->str);
-#else
-  /* TODO(ensonic): report format is still unstable */
-  gst_tracer_log_trace (gst_structure_new ("interlatency",
-          "from_pad", G_TYPE_STRING, src,
-          "to_pad", G_TYPE_STRING, sink,
-          "time", G_TYPE_STRING, time_string->str, NULL));
-#endif
   do_print_interlatency_event (INTERLATENCY_EVENT_ID, src, sink, time);
 
   g_string_free (time_string, TRUE);
@@ -274,9 +260,6 @@ gst_interlatency_tracer_class_init (GstInterlatencyTracerClass * klass)
 
   gobject_class->dispose = gst_interlatency_tracer_dispose;
 
-  /* announce trace formats */
-  /* *INDENT-OFF* */
-#ifdef GST_STABLE_RELEASE
   tr_interlatency = gst_tracer_record_new ("interlatency.class",
       "from_pad", GST_TYPE_STRUCTURE, gst_structure_new ("scope",
           "type", G_TYPE_GTYPE, G_TYPE_STRING,
