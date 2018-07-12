85eade98f413dc190b374ad1cda199e7467f1fd3
Angel Phillips <angel.phillips@ridgerun.com>
2018-07-12 12:13:29 -0600

    Simplify object by using gobject's shorthand macros for scheduletime tracer

--- plugins/tracers/gstscheduletime.h
+++ plugins/tracers/gstscheduletime.h
@@ -25,45 +25,10 @@
 #include <gst/gsttracer.h>
 
 G_BEGIN_DECLS
-#define GST_TYPE_SCHEDULETIME_TRACER \
-  (gst_scheduletime_tracer_get_type())
-#define GST_SCHEDULETIME_TRACER(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_SCHEDULETIME_TRACER,GstScheduletimeTracer))
-#define GST_SCHEDULETIME_TRACER_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_SCHEDULETIME_TRACER,GstScheduletimeTracerClass))
-#define GST_IS_SCHEDULETIME_TRACER(obj) \
-  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_SCHEDULETIME_TRACER))
-#define GST_IS_SCHEDULETIME_TRACER_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_SCHEDULETIME_TRACER))
-#define GST_SCHEDULETIME_TRACER_CAST(obj) ((GstScheduletimeTracer *)(obj))
-typedef struct _GstSchedulePad GstSchedulePad;
-typedef struct _GstScheduletimeTracer GstScheduletimeTracer;
-typedef struct _GstScheduletimeTracerClass GstScheduletimeTracerClass;
 
-/**
- * GstScheduletimeTracer:
- *
- * Opaque #GstScheduletimeTracer data structure
- */
-
-struct _GstSchedulePad
-{
-  GstPad *pad;
-  GstClockTime previous_time;
-};
-
-struct _GstScheduletimeTracer
-{
-  GstTracer parent;
-  GHashTable *schedule_pads;
-};
-
-struct _GstScheduletimeTracerClass
-{
-  GstTracerClass parent_class;
-};
-
-G_GNUC_INTERNAL GType gst_scheduletime_tracer_get_type (void);
+#define GST_TYPE_SCHEDULETIME_TRACER (gst_scheduletime_tracer_get_type ())
+G_DECLARE_FINAL_TYPE (GstScheduletimeTracer, gst_scheduletime_tracer, GST, SCHEDULETIME_TRACER, GstTracer)
 
 G_END_DECLS
+
 #endif /* __GST_SCHEDULETIME_TRACER_H__ */
