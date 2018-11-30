603924061975b256839c3b63dcb9e7a9f213e025
rrcarlosrodriguez <carlos.rodriguez@ridgerun.com>
2018-11-30 11:46:42 -0600

    Remove unused macros

--- scripts/graphics/plot_bitrate.m
+++ scripts/graphics/plot_bitrate.m
@@ -1,14 +1,5 @@
 #! /usr/bin/octave -qf
 
-# Configuration
-RESULT = 0;
-FONTSIZE = 14;
-LINEWIDTH = 1;
-
-# Constants
-TRUE = 1;
-FALSE = 0;
-
 [pad_name_list, timestamp_mat, bitrate_mat] = load_serie_timestamp_value('bitrate.mat');
 
 if ((1 == length(timestamp_mat)) && (0 == timestamp_mat))
