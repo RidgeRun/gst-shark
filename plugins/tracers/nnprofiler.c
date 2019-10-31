c6823d1dd521006476b2ea15aacdbbbdda708749
KRKimDongWoo <dhsodhao52@naver.com>
2019-11-01 00:38:21 +0900

    Elements are now adding to lists

--- plugins/tracers/nnprofiler.c
+++ plugins/tracers/nnprofiler.c
@@ -13,7 +13,7 @@
 #define RUNTIME_MINUTE 1
 #define NUMBER_OF_CPU 4
 #define NUMBER_OF_NN 20
-#define NUMBER_OF_EDGE 20*2
+#define NUMBER_OF_EDGE 30*2
 #define COL_SCALE 21
 
 //global_variables
@@ -23,19 +23,21 @@ int ncurses_col_current=0;
 float ncurses_cpuload[NUMBER_OF_CPU];
 
 struct ncurses_NN {
-    bool used;
-    char name[20];
-    unsigned long proctime;
-    unsigned long framerate;
+    gboolean used;
+    gchar name[30];
+    guint64 proctime;
+    guint64 framerate;
 
 }ncurses_NN[NUMBER_OF_NN];
+int max_element = 0;
 
 struct ncurses_NN_edge {
-    bool used; 
-    char sname[20];
-    char dname[20];
-    unsigned long latency;
+    gboolean used; 
+    gchar sname[30];
+    gchar dname[30];
+    guint64 latency;
 }ncurses_NN_edge[NUMBER_OF_EDGE];
+int max_edge = 0;
 
 void *curses_loop(void* arg);
 
@@ -62,9 +64,7 @@ void ncurses_initialize(void) {
 	curs_set(0);
 	noecho();
 }
-// int main() {
-// 	gst_nnprofiler_init();
-// }
+
 void* curses_loop(void *arg){
     ncurses_initialize();
 
@@ -75,7 +75,7 @@ void* curses_loop(void *arg){
         ncurses_col_current = 0;
         timeout(0);
         key_in = getch();
-        if (key_in == 'q' || key_in == 'Q' || iter >= 1000/TIMESCALE * 60 * RUNTIME_MINUTE) break;
+        if (key_in == 'q' || key_in == 'Q') break;
         clear();
 
         // draw
@@ -114,11 +114,11 @@ void* curses_loop(void *arg){
         mvprintw(ncurses_row_current++, ncurses_col_current, "Proctime&Framerate : ");
         i=0;
 
-        while(i<NUMBER_OF_NN && ncurses_NN[i].used ) {
+        while(i<max_element && ncurses_NN[i].used ) {
             mvprintw(ncurses_row_current+1, ncurses_col_current, "NeuralNetwork      |");
             mvprintw(ncurses_row_current+2, ncurses_col_current, "Proctime           |");
             mvprintw(ncurses_row_current+3, ncurses_col_current, "Framerate          |");
-            while(i<NUMBER_OF_NN && ncurses_col_current<COL_MAX && ncurses_NN[i].used) {
+            while(i<max_element && ncurses_col_current<COL_MAX && ncurses_NN[i].used) {
                 ncurses_col_current += COL_SCALE;
                 mvprintw(ncurses_row_current+1, ncurses_col_current, "%20s|", ncurses_NN[i].name);
                 mvprintw(ncurses_row_current+2, ncurses_col_current, "%18lums|", ncurses_NN[i].proctime);
@@ -140,11 +140,11 @@ void* curses_loop(void *arg){
         //Interlatency
         mvprintw(ncurses_row_current++, ncurses_col_current, "Interlatency : ");
         i=0;
-        while(i<NUMBER_OF_EDGE && ncurses_NN_edge[i].used) {
+        while(i<max_edge && ncurses_NN_edge[i].used) {
             mvprintw(ncurses_row_current+1, ncurses_col_current, "Source             |");
             mvprintw(ncurses_row_current+2, ncurses_col_current, "Destination        |");
             mvprintw(ncurses_row_current+3, ncurses_col_current, "latency            |");
-            while(i<NUMBER_OF_EDGE && ncurses_col_current<COL_MAX && ncurses_NN_edge[i].used) {
+            while(i < max_edge && ncurses_col_current<COL_MAX && ncurses_NN_edge[i].used) {
                 ncurses_col_current += COL_SCALE;
                 mvprintw(ncurses_row_current+1, ncurses_col_current, "%20s|", ncurses_NN_edge[i].sname);
                 mvprintw(ncurses_row_current+2, ncurses_col_current, "%20s|", ncurses_NN_edge[i].dname);
@@ -200,21 +200,19 @@ update_cpuusage_event (guint32 cpunum, gfloat * cpuload)
 void 
 update_proctime_event (gchar * elementname, guint64 time) 
 {
-	for(int i=0; i<NUMBER_OF_NN; i++) {
-		if(strcmp(ncurses_NN[i].name, (char*)elementname)) {
-			ncurses_NN[i].used = true;
-			ncurses_NN[i].proctime = (unsigned long)time;
+	for(int i=0; i<max_element; i++) {
+		if(strcmp(ncurses_NN[i].name, elementname) == 0) {
+			ncurses_NN[i].used = TRUE;
+			ncurses_NN[i].proctime = time;
 			return;
 		}
 	}
-	for(int i=0; i<NUMBER_OF_NN; i++) {
-		if(!ncurses_NN[i].used) {
-			ncurses_NN[i].used = true;
-			strcpy(ncurses_NN[i].name, (char*)elementname);
-			ncurses_NN[i].proctime= (unsigned long)time;
-			ncurses_NN[i].framerate= (unsigned long)0;
-		}
-	}
+	strcpy(ncurses_NN[max_element].name, elementname);
+	ncurses_NN[max_element].used = TRUE;
+	ncurses_NN[max_element].proctime = time;
+	ncurses_NN[max_element].framerate = 0;
+	max_element += 1;
+	
 	return;
 	//TODO: Update output with proctime
 	// input: elementname (name of the element)
@@ -224,21 +222,19 @@ update_proctime_event (gchar * elementname, guint64 time)
 void 
 update_framerate_event (gchar * elementname, guint64 fps) 
 {
-	for(int i=0; i<NUMBER_OF_NN; i++) {
-		if(strcmp(ncurses_NN[i].name, (char*)elementname)) {
-			ncurses_NN[i].used = true;
-			ncurses_NN[i].framerate = (unsigned long)fps;
+	for(int i=0; i<max_element; i++) {
+		if(strcmp(ncurses_NN[i].name, elementname) == 0) {
+			ncurses_NN[i].used = TRUE;
+			ncurses_NN[i].framerate = fps;
 			return;
 		}
 	}
-	for(int i=0; i<NUMBER_OF_NN; i++) {
-		if(!ncurses_NN[i].used) {
-			ncurses_NN[i].used = true;
-			strcpy(ncurses_NN[i].name, (char*)elementname);
-			ncurses_NN[i].proctime= (unsigned long)0;
-			ncurses_NN[i].framerate= (unsigned long)fps;
-		}
-	}
+	ncurses_NN[max_element].used = TRUE;
+	strcpy(ncurses_NN[max_element].name, elementname);
+	ncurses_NN[max_element].proctime = 0;
+	ncurses_NN[max_element].framerate = fps;
+	max_element += 1;
+
 	return;
 	//TODO: Update output with framerate
 	// input: elementname (name of the element)
@@ -253,20 +249,18 @@ update_interlatency_event (gchar * originpad,
 	// input: originpad (name of the pad buffer started)
 	//        destinationpad (name of the pad buffer arrived)
 	//        time (interlatency between two pads)
-	for(int i=0; i<NUMBER_OF_EDGE; i++) {
-		if(strcmp(ncurses_NN_edge[i].sname, (char*)originpad) && strcmp(ncurses_NN_edge[i].dname, (char*)destinationpad) ) {
-			ncurses_NN_edge[i].used = true;
-			ncurses_NN_edge[i].latency = (unsigned long)time;
+	for(int i=0; i < max_edge; i++) {
+		if((strcmp(ncurses_NN_edge[i].sname, originpad) == 0) && (strcmp(ncurses_NN_edge[i].dname, destinationpad) == 0)) {
+			ncurses_NN_edge[i].used = TRUE;
+			ncurses_NN_edge[i].latency = time;
 			return;
 		}
 	}
-	for(int i=0; i<NUMBER_OF_EDGE; i++) {
-		if(!ncurses_NN_edge[i].used) {
-			ncurses_NN_edge[i].used=true;
-			strcpy(ncurses_NN_edge[i].sname, (char*)originpad);
-			strcpy(ncurses_NN_edge[i].dname, (char*)destinationpad);
-			ncurses_NN_edge[i].latency = (unsigned long)time;
-		}
-	}
+	ncurses_NN_edge[max_edge].used = TRUE;
+	strcpy(ncurses_NN_edge[max_edge].sname, originpad);
+	strcpy(ncurses_NN_edge[max_edge].dname, destinationpad);
+	ncurses_NN_edge[max_edge].latency = time;
+	max_edge += 1;
+
 	return;
 }
