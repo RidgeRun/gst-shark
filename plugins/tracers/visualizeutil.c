c7935dc1ab786b6f62bfa71a85afde8ee92a4036
KRKimDongWoo <dhsodhao52@naver.com>
2019-12-16 00:21:25 +0900

    Added select function for pad

--- plugins/tracers/visualizeutil.c
+++ plugins/tracers/visualizeutil.c
@@ -25,23 +25,37 @@ int row_offset=0; //for scrolling
 // NCurses color scheme
 #define INVERT_PAIR	1
 #define TITLE_PAIR	2
-#define SELECT_PAIR 3
+#define SELECT_ELEMENT_PAIR 3
+#define SELECT_PAD_PAIR 4
 
 // View Mode
 char currentModeText[14];
-int currentMode=0; //0-list, 1-matrix, 2-graph
 
-GList * keyIterator=NULL;
+GList * elementIterator = NULL;
+GList * padIterator = NULL;
+gchar * pairPad = NULL;
+gchar * pairElement = NULL;
 
 // Iterator for Hashtable
 void print_pad(gpointer key, gpointer value, gpointer user_data) {
 	gchar * name = (gchar *) key;
 	PadUnit * data = (PadUnit *) value;
+	gboolean * selected = (gboolean *) user_data;
 
-	mvprintw(row_offset+row_current, 4, "%s", name);
+	if(padIterator && *selected && strcmp(key, padIterator->data) == 0) {
+		attron(A_BOLD); attron(COLOR_PAIR(SELECT_PAD_PAIR));
+		mvprintw(row_offset+row_current, 4, "%s", name);
+		attroff(A_BOLD); attroff(COLOR_PAIR(SELECT_PAD_PAIR));
+		
+		pairElement = GST_OBJECT_NAME (GST_OBJECT_PARENT (data->element));
+		pairPad = GST_OBJECT_NAME (data->element);
+	}
+	else {
+		mvprintw(row_offset+row_current, 4, "%s", name);
+	}
 
 	mvprintw(row_offset+row_current, ELEMENT_NAME_MAX * 4 + 2, 
-			"%20d", data->framerate);
+			"%20f", data->datarate);
 	row_current++;
 }
 
@@ -49,16 +63,20 @@ void print_pad(gpointer key, gpointer value, gpointer user_data) {
 void print_element(gpointer key, gpointer value, gpointer user_data) {
 	gchar * name = (gchar *) key;
 	ElementUnit * data = (ElementUnit *) value;
-	
-	attron(A_BOLD);
-	g_return_if_fail(keyIterator);
-	if(keyIterator && strcmp(key, keyIterator->data) == 0)
-		attron(COLOR_PAIR(SELECT_PAIR));
-	mvprintw(row_offset+row_current, 0, "%s", name);
-	if(keyIterator && strcmp(key, keyIterator->data) == 0)
-		attroff(COLOR_PAIR(SELECT_PAIR));
-	attroff(A_BOLD);
+	gboolean * selected = g_malloc(sizeof(gboolean *));
 
+	attron(A_BOLD);
+	if(elementIterator && strcmp(key, elementIterator->data) == 0) {
+		attron(COLOR_PAIR(SELECT_ELEMENT_PAIR));
+		mvprintw(row_offset+row_current, 0, "%s", name);
+		attroff(COLOR_PAIR(SELECT_ELEMENT_PAIR));
+		*selected = TRUE;
+	}
+	else {
+		mvprintw(row_offset+row_current, 0, "%s", name);
+		*selected = FALSE;
+	}	
+	attroff(A_BOLD);
 
 	mvprintw(row_offset+row_current, ELEMENT_NAME_MAX,
 			"%20ld %20f %17d/%2d", 
@@ -68,22 +86,9 @@ void print_element(gpointer key, gpointer value, gpointer user_data) {
 			data->max_queue_level);
 	row_current++;
 	
-	g_hash_table_foreach(data->pad, (GHFunc) print_pad, NULL);	
-}
+	g_hash_table_foreach(data->pad, (GHFunc) print_pad, selected);
 
-void print_connection(gpointer key, gpointer value, gpointer user_data) {
-	/*
-	char * connection_key = strdup((char *) key);
-	char * sname = strtok(connection_key, " ");
-	char * dname = strtok(NULL, " ");
-	ProfilerConnection * data = (ProfilerConnection *) value;
-
-	mvprintw(row_current, 0, "%s", sname);
-	mvprintw(row_current, ELEMENT_NAME_MAX, "%s", dname);
-	mvprintw(row_current, ELEMENT_NAME_MAX * 2,
-			"%18dns", data->interlatency);
-	row_current++;
-	*/
+	g_free(selected);	
 }
 
 void initialize(void) 
@@ -92,13 +97,13 @@ void initialize(void)
 	col_current = 0;
 	row_offset = 0; //for scrolling
 	strcpy(currentModeText, "list");
-	currentMode = 0;
 	initscr();
 	raw();
 	start_color();
 	init_pair(INVERT_PAIR, COLOR_BLACK, COLOR_WHITE);
 	init_pair(TITLE_PAIR, COLOR_BLUE, COLOR_BLACK);
-	init_pair(SELECT_PAIR, COLOR_RED, COLOR_BLACK);
+	init_pair(SELECT_ELEMENT_PAIR, COLOR_RED, COLOR_BLACK);
+	init_pair(SELECT_PAD_PAIR, COLOR_YELLOW, COLOR_BLACK);
 	keypad(stdscr, TRUE);
 	curs_set(0);
 	noecho();
@@ -128,7 +133,9 @@ void * curses_loop(void *arg)
     int key_in;
     int iter = 0;
     int i;
-	GList * element_key=NULL;
+	ElementUnit * element = NULL;
+	GList * element_key = NULL;
+	GList * pad_key = NULL;
 
 	printf("DONE INITIALIZE\n");
     initialize();
@@ -136,9 +143,13 @@ void * curses_loop(void *arg)
     while(1) {
 		if(element_key == NULL) {
 			element_key = g_hash_table_get_keys(packet->elements);
-			keyIterator = g_list_first(element_key);
+			elementIterator = g_list_last(element_key);
 		}
-			
+		else if(pad_key == NULL) {
+			element = g_hash_table_lookup(packet->elements, elementIterator->data);
+			pad_key = g_hash_table_get_keys(element->pad);
+			padIterator = g_list_last(pad_key);		
+		}
 
         row_current = 0;
         col_current = 0;
@@ -149,37 +160,36 @@ void * curses_loop(void *arg)
         if (key_in == 'q' || key_in == 'Q') break;
 	
 	switch(key_in) {//key value
-		case 'l':
-		case 'L':
-			if (currentMode != 0) { // mode switch
-				currentMode = 0; //list
-				strcpy(currentModeText, "list");
-				row_offset = 0; //reset offset
+		case 'w': //arrow right
+			if(g_list_next(elementIterator)) {
+				elementIterator = g_list_next(elementIterator);
+				element = g_hash_table_lookup(packet->elements, elementIterator->data);
+				pad_key = g_hash_table_get_keys(element->pad);
+				padIterator = g_list_last(pad_key);
 			}
 			break;
-		case 'm':
-		case 'M':
-			if (currentMode != 1) { // mode switch
-				currentMode = 1; //matrix
-				strcpy(currentModeText, "matrix");
-				row_offset = 0; //reset offset
+		case 's': //arrow left
+			if(g_list_previous(elementIterator)) {
+				elementIterator = g_list_previous(elementIterator);
+				element = g_hash_table_lookup(packet->elements, elementIterator->data);
+				pad_key = g_hash_table_get_keys(element->pad);
+				padIterator = g_list_last(pad_key);
 			}
 			break;
-		case 'g':
-		case 'G':
-			if (currentMode != 2) { // mode switch
-				currentMode = 2; //graph
-				strcpy(currentModeText, "graph");
-				row_offset = 0; //reset offset
+		case 'a':
+			if(g_list_next(padIterator)) {
+				padIterator = g_list_next(padIterator);
 			}
 			break;
-		case 260: //arrow right
-			if(g_list_next(keyIterator))
-				keyIterator = g_list_next(keyIterator);
+		case 'd':
+			if(g_list_previous(padIterator)) {
+				padIterator = g_list_previous(padIterator);
+			}
 			break;
-		case 261: //arrow left
-			if(g_list_previous(keyIterator))
-				keyIterator = g_list_previous(keyIterator);
+		case 32:
+			if(element_key && pad_key) {
+				elementIterator = g_list_first(element_key);
+			}
 			break;
 		case 259: //arrow up
 			if (row_offset < 0) row_offset++;
@@ -194,105 +204,50 @@ void * curses_loop(void *arg)
 	//get time
 	tmp_t = time(NULL);
 	tm = *localtime(&tmp_t);
-        // draw
-        clear();
-		mvprintw(row_offset+row_current, 36, "key"); //for debug
-		mvprintw(row_offset+row_current, 40, "%08d",key_in); //for debug
+	// draw
+	clear();
+	mvprintw(row_offset+row_current, 36, "key"); //for debug
+	mvprintw(row_offset+row_current, 40, "%08d",key_in); //for debug
 	attron(A_BOLD);
 	mvprintw(row_offset+row_current, 63, "%4d-%02d-%02d %2d:%02d:%02d\n",
-		tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec); //time indicator
+			tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec); //time indicator
 	attron(COLOR_PAIR(INVERT_PAIR));
-        mvprintw(row_offset+row_current++, col_current, "Press 'q' or 'Q' to quit");
+	mvprintw(row_offset+row_current++, col_current, "Press 'q' or 'Q' to quit");
 	attroff(A_BOLD);
 	attroff(COLOR_PAIR(INVERT_PAIR));
-	mvprintw(row_offset+row_current, col_current, "Current mode: ");
-	mvprintw(row_offset+row_current, col_current + 14, currentModeText);
-	mvprintw(row_offset+row_current++, col_current + 28, "('l': list, 'm': matrix, 'g': graph)");
+	print_line(&row_current, &col_current);
+	
+	//CPU Usage
+	attron(A_BOLD);
+	attron(COLOR_PAIR(TITLE_PAIR));
+	mvprintw(row_offset+row_current++, col_current, "CPU Usage");
+	attroff(A_BOLD);
+	attroff(COLOR_PAIR(TITLE_PAIR));
+	i=0;
+	while(i < packet->cpu_num) {
+		attron(A_BOLD);
+		mvprintw(row_offset+row_current, col_current, "CPU%2d", i);
+		attroff(A_BOLD);
+		mvprintw(row_offset+row_current++, col_current+7+4, "%3.1f%%", packet->cpu_load[i]);
+		i++;
+	}
+	
 	print_line(&row_current, &col_current);
 
-	switch(currentMode) {
-		case 0: //list
-			//CPU Usage
-			attron(A_BOLD);
-			attron(COLOR_PAIR(TITLE_PAIR));
-			mvprintw(row_offset+row_current++, col_current, "CPU Usage");
-			attroff(A_BOLD);
-			attroff(COLOR_PAIR(TITLE_PAIR));
-			i=0;
-			while(i < packet->cpu_num) {
-				attron(A_BOLD);
-				mvprintw(row_offset+row_current, col_current, "CPU%2d", i);
-				attroff(A_BOLD);
-				mvprintw(row_offset+row_current++, col_current+7+4, "%3.1f%%", packet->cpu_load[i]);
-				i++;
-			}
+	// Proctime & Framerate
+	attron(A_BOLD);
+	attron(COLOR_PAIR(TITLE_PAIR));
+	mvprintw(row_offset+row_current, 0, "ElementName");
+	mvprintw(row_offset+row_current++, ELEMENT_NAME_MAX,
+			"%20s %20s %20s %20s", "Proctime(ns)", "Avg_proctime(ns)", "queuelevel", "Framerate(fps)");	
+	attroff(COLOR_PAIR(TITLE_PAIR));
+	attroff(A_BOLD);
+	
+	g_hash_table_foreach(packet->elements, (GHFunc) print_element, NULL);
 
-			print_line(&row_current, &col_current);
-
-			// Proctime & Framerate
-			attron(A_BOLD);
-			attron(COLOR_PAIR(TITLE_PAIR));
-			mvprintw(row_offset+row_current, 0, "ElementName");
-			mvprintw(row_offset+row_current++, ELEMENT_NAME_MAX,
-				   "%20s %20s %20s %20s", "Proctime(ns)", "Avg_proctime(ns)", "queuelevel", "Framerate(fps)");	
-			attroff(COLOR_PAIR(TITLE_PAIR));
-			attroff(A_BOLD);
-
-			g_hash_table_foreach(packet->elements, (GHFunc) print_element, NULL);
-			/*
-				// Interlatency
-				mvprintw(row_offset+row_current, 0, "Source");
-				mvprintw(row_offset+row_current, ELEMENT_NAME_MAX, "Destination");
-				mvprintw(row_offset+row_current++, ELEMENT_NAME_MAX * 2, 
-						"%20s", "Interlatency");
-
-				g_hash_table_foreach(CONNECTIONS(packet), (GHFunc) print_connection, NULL);
-			*/
-			break;
-		case 1: //matrix
-			//CPU Usage
-			mvprintw(row_offset+row_current++, col_current, "CPU Usage : ");
-
-			i=0;
-			while(i < packet->cpu_num) {
-			    mvprintw(row_offset+row_current+1, col_current, "CPU                |");
-			    mvprintw(row_offset+row_current+2, col_current, "CPU Usage          |");
-			    while(i < packet->cpu_num) {
-				col_current += COL_SCALE;
-				mvprintw(row_offset+row_current+1, col_current, "%20d|", i);
-				mvprintw(row_offset+row_current+2, col_current, "%19f%%|", packet->cpu_load[i]);
-				i++;
-			    }
-			    row_current += 3;
-			    col_current  = 0; 
-			}
-
-			print_line(&row_current, &col_current);
-
-			// Proctime & Framerate
-			mvprintw(row_offset+row_current, 0, "ElementName");
-			mvprintw(row_offset+row_current++, ELEMENT_NAME_MAX,
-				   "%20s %20s %20s", "Proctime(ns)", "Avg_proc(ns)", "queuelevel", "Framerate(fps)");	
-
-			g_hash_table_foreach(packet->elements, (GHFunc) print_element, NULL);
-			/*
-				// Interlatency
-				mvprintw(row_offset+row_current, 0, "Source");
-				mvprintw(row_offset+row_current, ELEMENT_NAME_MAX, "Destination");
-				mvprintw(row_offset+row_current++, ELEMENT_NAME_MAX * 2, 
-						"%20s", "Interlatency");
-
-				g_hash_table_foreach(CONNECTIONS(packet), (GHFunc) print_connection, NULL);
-			*/
-			break;
-		case 2:	//graph
-			break;
-		default:
-			break;
-	}
-        iter++;
-        refresh();
-        milsleep(TIMESCALE / 4);
+    iter++;
+    refresh();
+    milsleep(TIMESCALE / 4);
 		
     }
     endwin();
