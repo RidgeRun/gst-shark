160983bd80ba5104d9aa9a9f649ba8c241eece61
Michael Gruner <michael.gruner@ridgerun.com>
2020-08-12 17:10:53 -0600

    Initialize barebones babeltrace plug-in

--- plugins/tracers/gstctf.c
+++ plugins/tracers/gstctf.c
@@ -23,3 +23,35 @@
 #endif
 
 #include "gstctf.h"
+
+#include <babeltrace2/babeltrace.h>
+
+static bt_message_iterator_class_next_method_status
+gst_ctf_iterator_next (bt_self_message_iterator * self_message_iterator,
+    bt_message_array_const messages, uint64_t capacity, uint64_t * count)
+{
+  bt_message_iterator_class_next_method_status status =
+      BT_MESSAGE_ITERATOR_CLASS_NEXT_METHOD_STATUS_OK;
+
+
+  return status;
+}
+
+/* Define plugin and source component */
+BT_PLUGIN_MODULE ();
+BT_PLUGIN (gst);
+BT_PLUGIN_SOURCE_COMPONENT_CLASS (tracers, gst_ctf_iterator_next);
+
+/* Plugin metadata */
+BT_PLUGIN_AUTHOR ("RidgeRun <support@ridgerun.com>");
+BT_PLUGIN_DESCRIPTION ("GStreamer tracing subsystem");
+BT_PLUGIN_LICENSE (GST_SHARK_LICENSE);
+BT_PLUGIN_VERSION (GST_VERSION_MAJOR, GST_VERSION_MINOR,
+    GST_VERSION_MICRO, GST_VERSION_NANO);
+
+/* Component metadata */
+BT_PLUGIN_SOURCE_COMPONENT_CLASS_DESCRIPTION (tracers,
+    "Connect to GStreamer tracers");
+BT_PLUGIN_SOURCE_COMPONENT_CLASS_HELP (tracers,
+    "See https://gstreamer.freedesktop.org/documentation/additional/"
+    "design/tracing.html for more details");
